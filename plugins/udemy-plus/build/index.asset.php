9aa1e304814be38234110484e9db05ce20ddf033
Imon <contact@imonweb.co.uk>
2023-07-22 10:05:58 +0100

    udemy plus plugin

---
+++ plugins/udemy-plus/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5e700b027a8ded40dffa');
---
+++ plugins/udemy-plus/index.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      8.0
+ * Author:            John Doe
+ * Author URI:        https://udemy.com
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+if(!function_exists('add_action')){
+  echo 'Seems like you stumbled here by accident. 😛';
+  exit;
+}
