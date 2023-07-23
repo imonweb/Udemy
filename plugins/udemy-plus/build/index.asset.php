a0226d4c0c9b5d1e61dc8fc5e2f7923ce397c466
Imon <contact@imonweb.co.uk>
2023-07-23 23:56:39 +0100

    block

--- plugins/udemy-plus/build/index.asset.php
+++ plugins/udemy-plus/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'e1d468635a3d042a0c78');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '88586775f9cc901847d6');
