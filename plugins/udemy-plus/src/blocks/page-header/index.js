6f79ff53a1b5431ff051af25427580a422d7bffd
Imon <contact@imonweb.co.uk>
2023-07-26 00:28:49 +0100

    toggle control component

--- plugins/udemy-plus/src/blocks/page-header/index.js
+++ plugins/udemy-plus/src/blocks/page-header/index.js
@@ -1,21 +1,42 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { 
-  useBlockProps
+  useBlockProps, RichText, InspectorControls
 } from '@wordpress/block-editor';
+import { PanelBody, ToggleControl } from '@wordpress/components'
+import { __ } from '@wordpress/i18n'
 import icons from '../../icons.js'
 import './main.css'
 
 registerBlockType('udemy-plus/page-header', {
   icon: icons.primary,
-	edit() {
+	edit({attributes, setAttributes}) {
+    const { content, showCategory } = attributes
     const blockProps = useBlockProps();
 
     return (
       <>
-        <div { ...blockProps }>
-          Page Header
+      <InspectorControls>
+        <PanelBody title={__('General', 'udemy-plus')}>
+          <ToggleControl 
+            label={__('Show Category', 'udemy-plus')}
+            checked={showCategory}
+            onChange={showCategory => setAttributes({ showCategory })}
+          />
+        </PanelBody>
+      </InspectorControls>
+        <div {...blockProps}>
+          <div className="inner-page-header">
+          <RichText 
+            tagName="h1"
+            placeholder={__("Heading", "udemy-plus")}
+            value={content}
+            onChange={content => setAttributes({ content })}
+          />
+          </div>
         </div>
       </>
     );
   }
 });
+
+ 
