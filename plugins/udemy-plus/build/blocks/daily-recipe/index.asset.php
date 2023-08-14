f05a61927fc0d61223e70413ee51d72a370305f1
Imon <contact@imonweb.co.uk>
2023-08-14 23:52:39 +0100

    daily recipe

--- plugins/udemy-plus/build/blocks/daily-recipe/index.asset.php
+++ plugins/udemy-plus/build/blocks/daily-recipe/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '028d3c87546003b517ec');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b1e3430a8b449d1195be');
