91903c465909a332a00f1c32d42f784f2601a46a
Imon <contact@imonweb.co.uk>
2023-08-28 22:16:25 +0100

    v1

--- plugins/alert-box/build/index.asset.php
+++ plugins/alert-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acf89441707b242b828d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ca146bdc2dfda8bbd55e');
