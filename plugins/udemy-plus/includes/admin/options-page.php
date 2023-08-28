eca51fd97215b2e4473fce434c13860257a67a32
Imon <contact@imonweb.co.uk>
2023-08-29 00:20:46 +0100

    options page

--- plugins/udemy-plus/includes/admin/options-page.php
+++ plugins/udemy-plus/includes/admin/options-page.php
@@ -4,7 +4,68 @@ function up_plugins_options_page(){
 
   ?>
     <div class="wrap">
-      Hello World!
+      <h1><?php esc_html_e('Udemy Plus Settings', 'udemy-plus' ); ?></h1>
+      <form novalidate="novalidate">
+        <table class="form-table">
+          <tbody>
+            <!-- Open Graph Title -->
+            <tr>
+              <th>
+                <label for="up_og_title">
+                  <?php esc_html_e('Open Graph Title', 'udemy-plus'); ?>
+                </label>
+              </th>
+              <td>
+                <input name="up_og_title" type="text" id="up_og_title"
+                  class="regular-text" />
+              </td>
+            </tr>
+            <!-- Open Graph Image -->
+            <tr>
+              <th>
+                <label for="up_og_image">
+                  <?php esc_html_e('Open Graph Image', 'udemy-plus'); ?>
+                </label>
+              </th>
+              <td>
+                <input type="hidden" name="up_og_image" id="up_og_image" />
+                <img id="og-img-preview">
+                <a href="#" class="button-primary" id="og-img-btn">
+                  Select Image
+                </a>
+              </td>
+            </tr>
+            <!-- Open Graph Description -->
+            <tr>
+              <th>
+                <label for="up_og_description">
+                  <?php esc_html_e('Open Graph Description', 'udemy-plus'); ?>
+                </label>
+              </th>
+              <td>
+                <textarea 
+                  id="up_og_description" 
+                  name="up_og_description"
+                  class="large-text"
+                ></textarea>
+              </td>
+            </tr>
+            <!-- Enable Open Graph -->
+            <tr>
+              <th>
+                <?php esc_html_e('Open Graph', 'udemy-plus'); ?>
+              </th>
+              <td>
+              <label for="up_enable_og"> 
+                <input name="up_enable_og" type="checkbox" id="up_enable_og" 
+                  value="1" /> 
+                <span>Enable</span>
+              </label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     </div>
   <?php 
 }
